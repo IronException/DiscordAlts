c65c51b248410c4238f455fa573ac00ef65e72ae
IronException <mail-fuer-friedolin@web.de>
2019-11-01 22:22:26 +0100

    token

--- venv/Main.py
+++ venv/Main.py
@@ -1,7 +1,9 @@
 import discord
+import Config
+
 
 class MyClient(discord.Client):
-    
+
     async def on_ready(self):
         print('Logged on as', self.user)
 
@@ -13,7 +15,6 @@ class MyClient(discord.Client):
         if message.content == 'ping':
             await message.channel.send('pong')
 
+
 client = MyClient()
-client.run('token')
-
-
+client.run(Config.token)
