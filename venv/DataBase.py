91a648ede575bba118fa5e2d7e7a45e19cbc53c8
IronException <mail-fuer-friedolin@web.de>
2019-11-05 11:15:50 +0100

    how is python working

---
+++ venv/DataBase.py
@@ -0,0 +1,22 @@
+import json
+
+
+
+class player:
+    
+    def player():
+         
+    
+    # contains allvariables
+    
+    def addSelf(self, depo):
+        depo[self.name] = depo
+
+
+def load_data():
+    name_depo = {}
+    new player(json).addSelf(name_depo)
+
+def get_data(name):
+    # somehow search the db now...
+    return name_depo[name]
