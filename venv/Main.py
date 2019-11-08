b2e23a928db68d62c97c26679ffa2dad2ca923bb
IronException <mail-fuer-friedolin@web.de>
2019-11-08 12:44:19 +0100

    this is a good plan?

--- venv/Main.py
+++ venv/Main.py
@@ -7,14 +7,15 @@ from DataBase import load_data
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
+        # to get all the data so the bot can return something
         await load_data()
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
 
-        if message.content.startswith("!test"):
-            channel = message.channel
-            await channel.send("test")
+        for name in sb.get_names():
+            if message.content.contains(name):
+                await message.channel.send(db.get_embed_for(name, message.author, message.content))
 
 
 client = MyClient()
