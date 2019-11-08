544c938748554e3ec5329c0452b29faec37b7f57
IronException <mail-fuer-friedolin@web.de>
2019-11-08 13:00:09 +0100

    Update DataBase.py

--- venv/DataBase.py
+++ venv/DataBase.py
@@ -1,24 +1,35 @@
-import json
+import os, json
 
 
 class Player:
 
-    def __init__(self, json):
-        pass
+    def __init__(self, data):
+        allData = json.loads(data)
 
     # contains allvariables
 
     def add_self(self, depo):
         depo[self.name] = depo
 
+def get_files(dir):
+    json_files = [pos_json for pos_json in os.listdir(dir) if pos_json.endswith('.json')
+    print(json_files)  # for me this prints ['foo.json']
+    return json_files
+        
 
-def load_data():
+def load_data(dir):
+    files = get_files(dir)
     name_depo = {}
-    print("loafing")
-    pl = Player
-    pl.add_self(name_depo)
-
-
-def get_data(name):
+    print("loading")
+    for file in files:
+        Player(file).add_self()
+                  
+def get_names():
+    return name_depo.keys() # works?
+                  
+def get_embed_for(name, author, message):
     # somehow search the db now...
+                  # test author wether he has acces,..? / actually I have to sptest this for all people that do have access to that channel...
+                  # test message for more specifications (attribute + embedstyle)
     return name_depo[name]
+ 
