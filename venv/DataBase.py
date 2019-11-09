9d745cb9a91adda69228cd9787d8eef702e637c8
IronException <mail-fuer-friedolin@web.de>
2019-11-09 01:23:59 +0100

    need to fix subfolder jsons + load json file + act data handling then...

--- venv/DataBase.py
+++ venv/DataBase.py
@@ -4,6 +4,7 @@ import os, json
 class Player:
 
     def __init__(self, data):
+        print(data)
         allData = json.loads(data)
 
     # contains allvariables
@@ -11,25 +12,30 @@ class Player:
     def add_self(self, depo):
         depo[self.name] = depo
 
+
 def get_files(dir):
-    json_files = [pos_json for pos_json in os.listdir(dir) if pos_json.endswith('.json')
+    json_files = [pos_json for pos_json in os.listdir(dir) if pos_json.endswith('.json')]
     print(json_files)  # for me this prints ['foo.json']
     return json_files
-        
+
+
+global name_depo
+
 
 def load_data(dir):
     files = get_files(dir)
+
     name_depo = {}
     print("loading")
     for file in files:
         Player(file).add_self()
-                  
+
+
 def get_names():
-    return name_depo.keys() # works?
-                  
+    return name_depo.keys()  # works?
+
+
 def get_embed_for(name, author, message):
-    # somehow search the db now...
-                  # test author wether he has acces,..? / actually I have to sptest this for all people that do have access to that channel...
-                  # test message for more specifications (attribute + embedstyle)
+    # somehow search the db now... test author wether he has acces,..? / actually I have to sptest this for all
+    # people that do have access to that channel... test message for more specifications (attribute + embedstyle)
     return name_depo[name]
- 
--- venv/Main.py
+++ venv/Main.py
@@ -1,19 +1,19 @@
 import discord
 import Config
 
-from DataBase import load_data
+import DataBase as db
 
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
         # to get all the data so the bot can return something
-        await load_data()
+        await db.load_data(Config.filepath)
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
 
-        for name in sb.get_names():
+        for name in db.get_names():
             if message.content.contains(name):
                 await message.channel.send(db.get_embed_for(name, message.author, message.content))
 
